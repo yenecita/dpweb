d00a67afb277b9476b23d57c2c33d1772ddff10d
yenyhuaman7595@gmail.com <yenyhuaman7595@gmail.com>
2025-11-06 09:03:49 -0500

    actual

--- view/vista-cliente.php
+++ view/vista-cliente.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Catálogo de Productos</title>
-  <link rel="stylesheet" href="styles.css">
+  
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
